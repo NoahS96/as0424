7e6a7f2f8e800a5a90655b23379617693afcaf42
NoahS96 <noah2296@gmail.com>
2024-04-19 23:22:28 -0500

    Updated main method to account for Checkout constructor changes.

--- as0424/src/main/java/as0424/Main.java
+++ as0424/src/main/java/as0424/Main.java
@@ -1,5 +1,8 @@
 package main.java.as0424;
 
+import java.time.LocalDate;
+
+import main.java.as0424.exceptions.CheckoutException;
 import main.java.as0424.pojo.checkout.Checkout;
 import main.java.as0424.pojo.checkout.RentalAgreement;
 import main.java.as0424.pojo.tool.Chainsaw;
@@ -9,11 +12,16 @@ public class Main {
 	public static void main(String[] args) {
 		String toolCode = "CHNS";
 		String brand = "Stihl";
+		LocalDate checkoutDate = LocalDate.now();
 		
-		Checkout checkout = new Checkout(toolCode, 5, 20);
-		RentalAgreement rentalAgreement = new RentalAgreement(new Chainsaw(toolCode, brand), checkout);
-		
-		rentalAgreement.printAgreement();
+		try {
+			Checkout checkout = new Checkout(toolCode, 5, 20, checkoutDate);
+			RentalAgreement rentalAgreement = new RentalAgreement(new Chainsaw(toolCode, brand), checkout);
+			
+			rentalAgreement.printAgreement();
+		} catch (CheckoutException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
